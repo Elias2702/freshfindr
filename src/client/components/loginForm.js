fccc5cd6a4d9ce2e6ba34cdd7322f5998ea95b41
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2019-03-01 23:03:27 +0100

    update component login/map

---
+++ src/client/components/loginForm.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+export default class LoginFrom extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <form
+                    onSubmit={this.props.handleSubmit}
+                    className="loginForm">
+                    <input
+                        type="text"
+                        onChange={this.props.handleChange}
+                        placeholder="Email"
+                        name="email"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={this.props.handleChange}
+                    />
+                    <button
+                        type="submit"
+                        value="submit"
+                        className="loginButton">
+                        {"Login"}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+}
---
+++ src/client/components/mapSideBar.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import MapSideBar from "./mapSideBar";
+
+export default class MapSideBar extends React.Component {
+
+    render(){
+        return(
+            <div className="mapSideBarContainer">
+                
+            </div>
+        );
+    }
+}
