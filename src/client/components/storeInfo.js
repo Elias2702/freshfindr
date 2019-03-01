de14f3e227b5587608619251ba7d7b8aa671c8ac
kaskader <antoine.dannemark@gmail.com>
2019-03-02 00:59:55 +0100

    remove comment on index.js + app.js + start storeInfo comp

---
+++ src/client/components/storeInfo.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default function storeInfo(props) {
+    return (
+        <h1>
+            {"Hello, "}
+            {props.name}
+        </h1>
+    );
+}
