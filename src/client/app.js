5faa909c4b616e11cf547b75faa2b91b327fc655
badou <badou@pop-os.localdomain>
2019-03-01 22:14:02 +0100

    new component

--- src/client/app.js
+++ src/client/app.js
@@ -9,6 +9,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 
-import HelloWorld from "./components/hello";
+import Freshfindr from "./components/freshfindr";
 
-ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
+ReactDOM.render(<Freshfindr />, document.querySelector("#app"));
