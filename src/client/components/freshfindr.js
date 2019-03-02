f2aea0a9f28cf7175604e38f6c47e917027ebd5f
Elias Djenidi <44195464+Elias2702@users.noreply.github.com>
2019-03-02 05:52:22 +0100

    searchbar and searchbutton

--- src/client/components/freshfindr.js
+++ src/client/components/freshfindr.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import MapContainer from "./map";
+import SearchBar from "./searchbar";
 
 export default class Freshfindr extends React.Component {
     render() {
         return (
             <div className="container">
                 <MapContainer />
+                <SearchBar />
             </div>
         );
     }
---
+++ src/client/components/searchbar.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+
+export default class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            DisplaySearchBar: "",
+            ButtonClicked: "",
+        };
+
+        this.buttonIsClicked = this.buttonIsClicked.bind(this);
+    }
+
+    buttonIsClicked(e) {
+        e.preventDefault();
+        this.setState({
+            DisplaySearchBar: (
+                <input
+                    type="text"
+                    placeholder="Search our Awesome Database ;)"
+                    className="textarea"
+                />
+            ),
+            ButtonClicked: "buttonIsClicked",
+        });
+    }
+
+    render() {
+        return (
+            <div className="searchBar">
+                <form>
+                    {this.state.DisplaySearchBar}
+                    <input
+                        type="button"
+                        className={`searchButton ${this.state.ButtonClicked}`}
+                        onClick={this.buttonIsClicked}
+                    />
+                </form>
+            </div>
+        );
+    }
+}
