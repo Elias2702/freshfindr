c2cf2dabf81a837523edc3facce206da1dfcca64
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2019-03-02 11:21:03 +0100

    sidebar analytics

--- src/client/components/analyticsSidebar.js
+++ src/client/components/analyticsSidebar.js
@@ -4,7 +4,18 @@ export default class AnalyticsSidebar extends React.Component {
     render() {
         return (
             <div className="analyticsSidebarContainer">
-                <i className="fas fa-chart-line" />
+                <div className="sidebarElement">
+                    <i className="fas fa-chart-line" />
+                </div>
+                <div className="sidebarElement">
+                    <i class="far fa-user"/>
+                </div>
+                <div className="sidebarElement">
+                    <i class="fas fa-euro-sign"/>
+                </div>
+                <div className="sidebarElement">
+                    <h3>Support</h3>
+                </div>
             </div>
         );
     }
