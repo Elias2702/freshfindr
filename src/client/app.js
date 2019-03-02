53ebf7082c82fa3efb038c7138876463f4dc4c8a
Elias Djenidi <44195464+Elias2702@users.noreply.github.com>
2019-03-02 01:06:35 +0100

    some file cleanup

--- src/client/app.js
+++ src/client/app.js
@@ -1,11 +1,3 @@
-/* becodeorg/trouvkach
- *
- * /src/client/app.js - Client entry point
- *
- * coded by leny@BeCode
- * started at 15/02/2019
- */
-
 import * as React from "react";
 import ReactDOM from "react-dom";
 
