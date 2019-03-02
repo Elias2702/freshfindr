57b55e204958af95fccfce559ec357a8cacbca4e
Elias Djenidi <44195464+Elias2702@users.noreply.github.com>
2019-03-02 04:08:28 +0100

    Delete h1 in freshfindr

--- src/client/components/freshfindr.js
+++ src/client/components/freshfindr.js
@@ -5,8 +5,6 @@ export default class Freshfindr extends React.Component {
     render() {
         return (
             <div className="container">
-                <h1>{"Freshfindr Map"}</h1>
-                <hr />
                 <MapContainer />
             </div>
         );
